54a84cc176015f62ac008a6471cb65ee1af73bbb
harimohann <harimohan123@outlook.com>
2022-09-29 15:01:06 +0530

    this is lalith

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,32 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+module "sandboxes" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "lalith.aws1@outlook.com"
+    AccountName               = "sandbox-afts"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "lalith.aws1@outlook.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+
